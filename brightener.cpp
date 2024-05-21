0bb9d1ba3a4d2abef9e2a8bdbc6e3edc724d8e0a
Shetty, Pritham <pritham.shetty@philips.com>
2024-05-21 16:41:48 +0530

    Removing Duplicates

--- brightener.cpp
+++ brightener.cpp
@@ -12,23 +12,36 @@ uint8_t brightenPixel(uint8_t inputPixel, uint8_t brighteningGrayscale, int& att
     return brightened;
 }
 
+shared_ptr<Image> IteratePixelsToBrighten(shared_ptr<Image> inputImage,int& attenuatedPixelCount, std::function<int(int,int)> getBrightness)
+{        
+        auto brightenedImage =
+            make_shared<Image>(inputImage->m_rows, inputImage->m_columns,
+                [inputImage, &attenuatedPixelCount, getBrightness](uint8_t* initPixels) {
+                    for (int x = 0; x < inputImage->m_rows; x++) {
+                        for (int y = 0; y < inputImage->m_columns; y++) {
+                            int index = inputImage->pixelIndex(x, y);
+                            initPixels[index] =
+                                brightenPixel(inputImage->getPixel(x, y), getBrightness(x,y), attenuatedPixelCount);
+                        }
+                    }
+                });
+        return brightenedImage;
+}
+
 shared_ptr<Image> BrightenWholeImage(shared_ptr<Image> inputImage, int& attenuatedPixelCount) {
     // For brightening, we add a certain grayscale (25) to every pixel.
     // While brightening, some pixels may cross the max brightness. They are
     // called 'attenuated' pixels
+   
     attenuatedPixelCount = 0;
-    auto brightenedImage = 
-        make_shared<Image>(inputImage->m_rows, inputImage->m_columns,
-            [inputImage, &attenuatedPixelCount](uint8_t* initPixels) {
-            for (int x = 0; x < inputImage->m_rows; x++) {
-                for (int y = 0; y < inputImage->m_columns; y++) {
-                    int index = inputImage->pixelIndex(x, y);
-                    initPixels[index] =
-                        brightenPixel(inputImage->getPixel(x, y), 25, attenuatedPixelCount);
-                }
-            }
-        });
-    return brightenedImage;
+
+    return IteratePixelsToBrighten(
+        inputImage,
+        attenuatedPixelCount,
+        [](const int x, const int y) {
+            return 25; 
+        }
+    );
 }
 
 shared_ptr<Image> AddBrighteningImage(shared_ptr<Image> inputImage, shared_ptr<Image> imageToAdd,
@@ -37,17 +50,14 @@ shared_ptr<Image> AddBrighteningImage(shared_ptr<Image> inputImage, shared_ptr<I
     // if (imageToAdd->m_rows != m_inputImage->m_rows || imageToAdd->m_columns != m_inputImage->m_columns) {
     //     return false;
     // }
+
     attenuatedPixelCount = 0;
-    auto brightenedImage =
-        make_shared<Image>(inputImage->m_rows, inputImage->m_columns,
-            [inputImage, imageToAdd, &attenuatedPixelCount](uint8_t* initPixels) {
-            for (int x = 0; x < inputImage->m_rows; x++) {
-                for (int y = 0; y < inputImage->m_columns; y++) {
-                    int index = inputImage->pixelIndex(x, y);
-                    initPixels[index] =
-                        brightenPixel(inputImage->getPixel(x, y), imageToAdd->getPixel(x, y), attenuatedPixelCount);
-                }
-            }
-    });
-    return brightenedImage;
+
+    return IteratePixelsToBrighten(
+        inputImage,
+        attenuatedPixelCount,
+        [imageToAdd](const int x, const int y) {
+            return imageToAdd->getPixel(x, y);
+        }
+    );
 }
